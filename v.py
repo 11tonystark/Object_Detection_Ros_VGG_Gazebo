ff299368e0dd5ddccb619ee2530ab7cbc131852b
Tanmay Shukla <37925901+11tonystark@users.noreply.github.com>
2020-01-03 08:13:23 +0800

    final_model_vvg_ros

--- v.py
+++ v.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers.normalization import BatchNormalization
-#from keras.layers.convolutional import Conv2D,MaxPooling2D
-from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.layers.convolutional import Conv2D,MaxPooling2D
+#from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
 from keras.layers.core import Activation,Flatten,Dense,Dropout
 from keras import backend as k
 #c=conv2D+ReLU+BN
@@ -17,43 +17,37 @@ class tinyVGG:
         
 
         model.add(Conv2D(input_shape=input_shape,filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-       # model.add(Conv2D(filters=64,kernel_size=(3,3),padding="same", activation="relu"))
-        model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
+        
+        model.add(Conv2D(64,(3,3),padding='same'))
+        model.add(Activation('relu'))
         model.add(BatchNormalization(axis=channel_dim))
-        #model.add(Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"))
-        model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+        model.add(MaxPooling2D(pool_size=(3,3)))
         model.add(Dropout(0.25))
 
-        model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-       # model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
-       # model.add(Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"))
+        model.add(Conv2D(128,(3,3),padding='same'))
+        model.add(Activation('relu'))
+       
+        model.add(Conv2D(128,(3,3),padding='same'))
+        model.add(Activation('relu'))
         model.add(BatchNormalization(axis=channel_dim))
-        model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+        model.add(MaxPooling2D(pool_size=(3,3)))
         model.add(Dropout(0.25))
 
-        model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-       # model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-        #model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
+        model.add(Conv2D(256,(3,3),padding='same'))
+        model.add(Activation('relu'))
+      
+        model.add(Conv2D(256,(3,3),padding='same'))
+        model.add(Activation('relu'))
         model.add(BatchNormalization(axis=channel_dim))
-        model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+        model.add(MaxPooling2D(pool_size=(3,3)))
         model.add(Dropout(0.25))
-
-        model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-       # model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-       # model.add(Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"))
-        model.add(BatchNormalization(axis=channel_dim))
-        model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Dropout(0.25))
-
         
         model.add(Flatten())
         model.add(Dense(4096))
         model.add(Activation('relu'))
         model.add(BatchNormalization())
-        model.add(Dropout(0.5))
-        model.add(Dense(3))
-        model.add(Activation('softmax'))
-
-      
+        model.add(Dropout(0.25))
+        model.add(Dense(1))
+        model.add(Activation('sigmoid'))
+        
         return model
